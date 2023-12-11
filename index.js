e3a01a4a0494e6116a256c28d0f582d2efe5f3bc
Rei Terasawa <rei.terasawa0823@gmail.com>
2023-12-10 22:11:54 -0600

    create QR code from URL of the user input

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-const fs = require("fs");
+// const fs = require("fs");
+import fs from "fs";
+import inquirer from 'inquirer';
+import qr from "qr-image"
 
-fs.writeFile("message.txt", "hello from node.js", err =>{
-    if (err) throw err;
-    console.log("the file has been saved");
-})
+// https://www.google.com/
+inquirer
+  .prompt([
+    {
+        message: "Type in your URL",
+        name: "URL"
+    }
+  ])
+  .then((answers) => {
+    const url = answers.URL;
+    const qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream('qr.png'));
 
-fs.readFile("./message.txt", "utf8", (err, data) => {
-    if (err) throw err;
-    console.log(data);
-})
+  })
+
+
+// const generateName = require("sillyname");
+
+// fs.writeFile("message.txt", "hello from node.js", err =>{
+//     if (err) throw err;
+//     console.log("the file has been saved");
+// })
+
+// fs.readFile("./message.txt", "utf8", (err, data) => {
+//     if (err) throw err;
+//     console.log(data);
+// })
+
+// var sillyname = generateName();
+
+// console.log(sillyname);
