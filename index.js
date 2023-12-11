38424cfa70b6217fb67d8636f863f421b913f0f4
Rei Terasawa <rei.terasawa0823@gmail.com>
2023-12-10 20:31:22 -0600

    work on file system module

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// console.log("hello world");
+const fs = require("fs");
+
+fs.writeFile("message.txt", "hello from node.js", err =>{
+    if (err) throw err;
+    console.log("the file has been saved");
+})
+
+fs.readFile("./message.txt", "utf8", (err, data) => {
+    if (err) throw err;
+    console.log(data);
+})
